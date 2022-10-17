372ba742bb22b65cd942530acb370c3995715f9f
Joseph Jobbings <joseph.jobbings@kingfisher.com>
2022-10-17 10:44:51 +0100

    formatting

--- src/handlers/scheduled-event-logger.js
+++ src/handlers/scheduled-event-logger.js
@@ -6,7 +6,6 @@ export const scheduledEventLoggerHandler = async (event, context) => {
     // All log statements are written to CloudWatch by default. For more information, see
     // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
     console.info(JSON.stringify(event))
-
     try {
       // get access token from fluent
       let at = await getAccessToken()
@@ -22,7 +21,6 @@ export const scheduledEventLoggerHandler = async (event, context) => {
           console.debug(`id: ${fe.id}; type: ${fe.type}; category: ${fe.category}.`)
         })
       }
-
     } catch(err) {
       console.error('Error calling Fluent. Details : ', err)
     }
